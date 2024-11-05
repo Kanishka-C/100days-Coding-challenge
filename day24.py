9f342d03a6bd4ebcd03a80b40e3e7d5bcd74f6fd
Kanishka <kanishkachandradas11b@gmail.com>
2024-11-05 21:29:36 +0530

    Pyramid of stars

--- day24.py
+++ day24.py
@@ -1,4 +1,10 @@
-n=int(input('Enter a number : '))
+'''  
+   *   
+  ***
+ *****
+*******
+'''
+n=int(input('Enter number of rows: '))
 for i in range(0,n):
     for j in range(0,n-i-1):
         print(" ",end='')
