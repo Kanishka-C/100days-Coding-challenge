12e4a460598efccf3d0cd787b58181ba934407bb
Kanishka <kanishkachandradas11b@gmail.com>
2024-11-21 23:05:01 +0530

    Replace substring in a string

--- day40.py
+++ day40.py
@@ -1,6 +1,8 @@
 #Replace substring in a string
 string=input('Enter a string : ')
 remove_substring=input('Enter the substring to be removed : ')
-new_substring=input('Enter the new substring : ')
-index=string.find(remove_substring)
-print('The new string : ',string.replace(remove_substring,new_substring))
+if string.find(remove_substring)>0:
+    new_substring=input('Enter the new substring : ')
+    print('The new string : ',string.replace(remove_substring,new_substring))
+else:
+    print(remove_substring,' is not a part of ',string)
