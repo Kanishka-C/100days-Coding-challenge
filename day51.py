15a964d66922ccb49e7ae48a97759e6f6cd48fb4
Kanishka <kanishkachandradas11b@gmail.com>
2024-12-02 19:39:27 +0530

    Array sorting

--- day51.py
+++ day51.py
@@ -5,7 +5,5 @@ arr=list(map(int,input().split()))
 for i in range(size-1):
     for j in range(size-i-1):
         if arr[j]>arr[j+1]:
-            temp=arr[j]
-            arr[j]=arr[j+1]
-            arr[j+1]=temp
+            arr[j],arr[j+1]=arr[j+1],arr[j]
 print(arr)
