b6ed5524d3dc2e9b3316ed08a30ce9c206ca80ed
Kanishka <kanishkachandradas11b@gmail.com>
2024-12-02 19:32:24 +0530

    sum of positive square elements in array

---
+++ day51.py
@@ -0,0 +1,11 @@
+#Array sorting
+size=int(input('Enter the size of array : '))
+print('Enter the elements of the array : ')
+arr=list(map(int,input().split()))
+for i in range(size-1):
+    for j in range(size-i-1):
+        if arr[j]>arr[j+1]:
+            temp=arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
+print(arr)
