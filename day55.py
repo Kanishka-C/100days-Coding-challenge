621e4a289630d7ca2e4c141bb32f0fef16d9565a
Kanishka <kanishkachandradas11b@gmail.com>
2024-12-07 15:57:49 +0530

    Maximum scalar product of 2 vector arrays

---
+++ day55.py
@@ -0,0 +1,12 @@
+#Maximum scalar product of 2 vector arrays
+size=int(input('Enter the size of array : '))
+print('Enter the elements of array x : ')
+x=list(map(int,input().split()))
+print('Enter the elements of array y : ')
+y=list(map(int,input().split()))
+product=0
+x.sort(reverse=True)
+y.sort(reverse=True)
+for i in range(size):
+    product+=x[i]*y[i]
+print('Maximum scalar product = ',product)
